ef64fb1f3ed63c7c17d521f6b4b5a50cad6a5159
Chandresh Patidar <chandreshpatidar5@gmail.com>
2023-07-07 23:31:11 +0530

    Add navbar UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import Navbar from './components/Navbar';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
